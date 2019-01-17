159f9f6663ffd4a7d2f61444d9da21cc8c14bf4b
krystian.panek <krystian.panek@cognifide.com>
2019-01-17 14:35:28 +0100

    GAP upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,22 +44,6 @@ aem {
     }
 }
 
-fork {
-    config {
-        cloneFiles()
-        moveFiles(mapOf(
-                "/com/company/example/aem" to "/{{projectGroup|substitute('.', '/')}}/{{projectName}}/aem",
-                "/example" to "/{{projectName}}"
-        ))
-        replaceContents(mapOf(
-                "com.company.example.aem" to "{{projectGroup}}.{{projectName}}.aem",
-                "com.company.example" to "{{projectGroup}}.{{projectName}}",
-                "Example" to "{{projectLabel}}",
-                "example" to "{{projectName}}"
-        ))
-    }
-}
-
 tasks {
     withType<Test>().configureEach {
         failFast = true
@@ -76,3 +60,5 @@ tasks {
         }
     }
 }
+
+apply(from = "gradle/fork.gradle.kts")
---
+++ gradle/fork.gradle.kts
@@ -0,0 +1,46 @@
+import com.neva.gradle.fork.ForkExtension
+
+configure<ForkExtension> {
+    properties {
+        define(mapOf(
+                "adUsername" to { defaultValue = System.getProperty("user.name") },
+                "adPassword" to { password() },
+                "projectName" to {
+                    description = "Artifact 'name' coordinate (lowercase)"
+                    validator { lowercased(); alphanumeric() }
+                },
+                "projectLabel" to { description = "Nice project name (human-readable)" },
+                "projectGroup" to {
+                    description = "Java package in source code and artifact 'group' coordinate"
+                    validator { javaPackage(); notContains("projectName") }
+                },
+                "aemInstanceType" to {
+                    select("local", "remote")
+                    description = "local - instance will be created on local file system.\nremote - connecting to remote instance only."
+                },
+                "aemInstanceRunModes" to { text("nosamplecontent") },
+                "aemInstanceJvmOpts" to { text("-server -Xmx1024m -XX:MaxPermSize=256M -Djava.awt.headless=true") },
+                "aemInstanceAuthorHttpUrl" to {
+                    url("http://localhost:4502")
+                    description = "URL for accessing AEM author instance"
+                },
+                "aemInstancePublishHttpUrl" to {
+                    url("http://localhost:4503")
+                    description = "URL for accessing AEM publish instance"
+                }
+        ))
+    }
+    config {
+        cloneFiles()
+        moveFiles(mapOf(
+                "/com/company/example/aem" to "/{{projectGroup|substitute('.', '/')}}/{{projectName}}/aem",
+                "/example" to "/{{projectName}}"
+        ))
+        replaceContents(mapOf(
+                "com.company.example.aem" to "{{projectGroup}}.{{projectName}}.aem",
+                "com.company.example" to "{{projectGroup}}.{{projectName}}",
+                "Example" to "{{projectLabel}}",
+                "example" to "{{projectName}}"
+        ))
+    }
+}
