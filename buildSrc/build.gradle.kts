9fdcee8906dc56a9cee816365be435a0e17dc1ec
krystian.panek <krystian.panek@cognifide.com>
2019-11-18 07:37:06 +0100

    AEM 6.5 align

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}
-
 repositories {
     mavenLocal()
     jcenter()
@@ -15,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:8.1.2")
+    implementation("com.cognifide.gradle:aem-plugin:9.0.0")
     implementation("com.neva.gradle:fork-plugin:4.0.0")
 }
 
