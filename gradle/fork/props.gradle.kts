5f208dc04860d1f3445b8d8340398ac326ab57f9
Krystian Panek <krystian.panek@cognifide.com>
2020-01-09 16:35:20 +0100

    Labels

--- gradle/fork/props.gradle.kts
+++ gradle/fork/props.gradle.kts
@@ -5,16 +5,19 @@ configure<ForkExtension> {
     properties {
         define("Instance type", mapOf(
                 "instanceType" to {
+                    label = "Type"
                     select("local", "remote")
                     description = "local - instance will be created on local file system\nremote - connecting to remote instance only"
                     controller { toggle(value == "local", "instanceRunModes", "instanceJvmOpts", "localInstance*") }
                 },
                 "instanceAuthorHttpUrl" to {
+                    label = "Author HTTP URL"
                     url("http://localhost:4502")
                     optional()
                     description = "URL for accessing AEM author instance"
                 },
                 "instancePublishHttpUrl" to {
+                    label = "Publish HTTP URL"
                     url("http://localhost:4503")
                     optional()
                     description = "URL for accessing AEM publish instance"
@@ -23,25 +26,36 @@ configure<ForkExtension> {
 
         define("Local instance", mapOf(
                 "localInstanceSource" to {
+                    label = "Source"
                     description = "Controls how instances will be created (from scratch, backup or automatically determined)"
                     select(Source.values().map { it.name.toLowerCase() }, Source.AUTO.name.toLowerCase())
                 },
                 "localInstanceQuickstartJarUri" to {
+                    label = "Quickstart URI"
                     description = "Quickstart JAR (cq-quickstart-x.x.x.jar)"
                 },
                 "localInstanceQuickstartLicenseUri" to {
+                    label = "Quickstart License URI"
                     description = "Quickstart license file (license.properties)"
                 },
                 "localInstanceBackupDownloadUri" to {
-                    description = "URL to backup file (SMB/SFTP/HTTP)"
+                    label = "Backup Download URI"
+                    description = "URL (SMB/SFTP/HTTP) to backup file"
                     optional()
                 },
                 "localInstanceBackupUploadUri" to {
-                    description = "URL to backup directory (SMB/SFTP)"
+                    label = "Backup Upload URI"
+                    description = "URL (SMB/SFTP) to directory containing backup files"
                     optional()
                 },
-                "instanceRunModes" to { text("local,nosamplecontent") },
-                "instanceJvmOpts" to { text("-server -Xmx2048m -XX:MaxPermSize=512M -Djava.awt.headless=true") }
+                "instanceRunModes" to {
+                    label = "Run Modes"
+                    text("local,nosamplecontent")
+                },
+                "instanceJvmOpts" to {
+                    label = "JVM Options"
+                    text("-server -Xmx2048m -XX:MaxPermSize=512M -Djava.awt.headless=true")
+                }
         ))
 
         define("File transfer", mapOf(
