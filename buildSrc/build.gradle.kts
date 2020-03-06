e1c179a1cdda9f72ea840b70be91cef5e9f45368
Krystian Panek <krystian.panek@cognifide.com>
2020-03-06 13:49:22 +0100

    GAP bump

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:11.0.1")
+    implementation("com.cognifide.gradle:aem-plugin:11.0.7")
     implementation("com.neva.gradle:fork-plugin:4.2.2")
 }
