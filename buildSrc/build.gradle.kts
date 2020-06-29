65cc86aa28b6cbab88cca27d8993cfd392a12c1a
Krystian Panek <krystian.panek@cognifide.com>
2020-06-29 09:38:21 +0200

    Upgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:14.0.2")
-    implementation("com.neva.gradle:fork-plugin:5.0.2")
+    implementation("com.cognifide.gradle:aem-plugin:14.0.3")
+    implementation("com.neva.gradle:fork-plugin:5.0.3")
     implementation("net.researchgate:gradle-release:2.6.0")
 }
