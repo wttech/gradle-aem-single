9d5cdfcd42fce99425b6fdf58772b643b56cb604
Krystian Panek <krystian.panek@cognifide.com>
2020-07-20 08:56:48 +0200

    Servlet fix

--- src/main/java/com/company/example/sling/core/HelloServlet.java
+++ src/main/java/com/company/example/sling/core/HelloServlet.java
@@ -1,4 +1,4 @@
-package com.company.sling.example.core;
+package com.company.example.sling.core;
 
 import java.io.IOException;
 import javax.servlet.Servlet;
@@ -14,6 +14,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
   service = Servlet.class,
   property = {
+    "sling.servlet.methods=GET",
     "sling.servlet.extensions=json",
     "sling.servlet.selectors=hello",
     "sling.servlet.resourceTypes=example/hello"
