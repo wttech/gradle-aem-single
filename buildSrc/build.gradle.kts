ecd824d77025fff08622892064a194c161bd6d34
Krystian Panek <krystian.panek@cognifide.com>
2020-08-24 21:46:39 +0200

    Hotfix

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:14.2.8")
+    implementation("com.cognifide.gradle:aem-plugin:14.2.9")
     implementation("com.neva.gradle:fork-plugin:5.0.5")
     implementation("net.researchgate:gradle-release:2.6.0")
 }
